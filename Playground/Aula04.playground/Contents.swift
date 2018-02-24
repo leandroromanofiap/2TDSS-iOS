b593f26f2246789ea4caf7da0524b20b1da21938
Leandro Romano <rm75244@fiap.com.br>
2018-02-23 23:01:11 -0300

    updateS

--- Playground/Aula04.playground/Contents.swift
+++ Playground/Aula04.playground/Contents.swift
@@ -62,3 +62,27 @@ var (street, number, zipCode) = address
 for i in 0..<invites.count {
     print("O convidado \(i + 1) é \(invites[i])")
 }
+
+// Operador unario
+prefix operator  >-
+prefix func >- (rhs: Int) -> Int {
+    return rhs * rhs
+}
+>-4
+
+postfix operator  -<
+postfix func -< (lhs: Int) -> Int {
+    return lhs * lhs * lhs
+}
+2-<
+
+infix operator  >-<
+func >-< (lhs: Int, rhs: Int) -> String {
+    return "\(lhs)\(rhs)"
+}
+20>-<18
+
+arc4random_uniform(60)
+
+
+

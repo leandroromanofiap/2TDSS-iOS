7b62c9f29d2f240968d95d13b3c7622b6a0c3cc5
Leandro Romano <rm75244@fiap.com.br>
2018-02-23 21:47:37 -0300

    Updates

--- Playground/Aula04.playground/Contents.swift
+++ Playground/Aula04.playground/Contents.swift
@@ -6,4 +6,20 @@ import Foundation
 //Coleções
 //Array: Coleção Ordenada
 
+var invites: [String] = [
+    "Jaqueline",
+    "Paula",
+    "Thiago",
+    "Camila",
+    "Raquel",
+]
 
+invites.append("Carol")
+
+invites.remove(at: 1)
+
+if let index = invites.index(of: "Camila") {
+    invites.remove(at: index)
+}
+
+invites.contains("Raquel")

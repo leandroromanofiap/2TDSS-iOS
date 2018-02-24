3fad1686d5fd6dabe02e918cc929317792de53ef
Leandro Romano <rm75244@fiap.com.br>
2018-02-23 22:37:04 -0300

    Updates

--- Playground/Aula04.playground/Contents.swift
+++ Playground/Aula04.playground/Contents.swift
@@ -23,3 +23,42 @@ if let index = invites.index(of: "Camila") {
 }
 
 invites.contains("Raquel")
+
+// Dictionary
+var states = [
+    "SP": "São Paulo",
+    "MG": "Minas Gerais",
+    "PA": "Pará"
+]
+
+if let state = states["PA"] {
+    print(state)
+}
+
+if let _ = states["SP"] {
+    print("Tem SP!")
+} else {
+    print("Não tem SP!")
+}
+
+states["RJ"] = "Rio de Janeiro"
+states["RJ"] = nil
+states
+
+states.count
+"FIAP".count
+
+for invite in invites {
+    print("\(invite) foi convidada para a festa.")
+}
+
+for state in states {
+    print("A chave \(state.key) se refere a \(state.value)")
+}
+
+let address: (street: String, number: Int, zipCode: String) = ("Av. Paulista", 1200, "01310-100")
+var (street, number, zipCode) = address
+
+for i in 0..<invites.count {
+    print("O convidado \(i + 1) é \(invites[i])")
+}

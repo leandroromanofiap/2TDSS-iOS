9878cd7850c0a5ab8196f05725e45e2d0790dc52
Leandro Romano <rm75244@fiap.com.br>
2018-03-09 21:32:03 -0300

    Arquivo para aula 05

---
+++ Playground/Aula06.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+func divide(a: Int, b: Int) -> Int {
+    return a / b
+}
+
+func multiply(a: Int, b: Int) -> Int {
+    return a * b
+}
+
+func subtract(a: Int, b: Int) -> Int {
+    return a - b
+}

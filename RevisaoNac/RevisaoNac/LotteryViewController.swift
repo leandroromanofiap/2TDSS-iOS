1bde7b6b19e4e62d3bad7cdaf0e2984830801db8
Leandro Romano <webleandroromano@gmail.com>
2018-04-27 22:29:50 -0300

    revisaonac

---
+++ RevisaoNac/RevisaoNac/LotteryViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LotteryViewController.swift
+//  RevisaoNac
+//
+//  Created by Leandro Romano on 27/04/18.
+//  Copyright © 2018 Leandro Romano. All rights reserved.
+//
+
+import UIKit
+
+class LotteryViewController: UIViewController {
+
+    @IBOutlet weak var lbLottery: UILabel!
+    var lottery: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lbLottery.text = "Esses são os jogos da \(lottery)"
+        title = lottery
+    }
+    
+    deinit {
+        
+    }
+
+}
---
+++ RevisaoNac/RevisaoNac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RevisaoNac
+//
+//  Created by Leandro Romano on 27/04/18.
+//  Copyright © 2018 Leandro Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? LotteryViewController else { return }
+        
+        guard let button = sender as? UIButton else { return }
+        
+        vc.lottery = button.currentTitle!
+    }
+    
+    @IBAction func showLottery(_ sender: UIButton) {
+        performSegue(withIdentifier: "segue", sender: sender)
+    }
+
+}
+

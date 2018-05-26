8269bf1702af6e6c8c5d331eec2dd9a89dd6df2a
Leandro Romano <webleandroromano@gmail.com>
2018-05-25 22:17:44 -0300

    update

---
+++ TableViewJson/TableViewJson/Console.swift
@@ -0,0 +1,23 @@
+//
+//  Console.swift
+//  TableViewJson
+//
+//  Created by Leandro Romano on 25/05/18.
+//  Copyright © 2018 Leandro Romano. All rights reserved.
+//
+
+import Foundation
+
+struct Console: Codable {
+    
+    let name: String
+    
+    let manufacturer: String
+    
+    let releaseDate: String
+    
+    let unitsSold: Double
+    
+    let image: String
+    
+}
---
+++ TableViewJson/TableViewJson/TableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  TableViewController.swift
+//  TableViewJson
+//
+//  Created by Leandro Romano on 25/05/18.
+//  Copyright © 2018 Leandro Romano. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var consoles: [Console] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadConsoles()
+    }
+    
+    func loadConsoles() {
+        let url = Bundle.main.url(forResource: "consoles", withExtension: "json")!
+        
+        do {
+            let data = try Data(contentsOf: url)
+            consoles = try JSONDecoder().decode([Console].self, from: data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return consoles.count
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

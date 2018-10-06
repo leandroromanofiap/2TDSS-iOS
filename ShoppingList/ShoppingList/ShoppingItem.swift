5e723a20a4a5e89e99c7e40996c2b1b4c67fa668
Leandro Romano <webleandroromano@gmail.com>
2018-10-05 22:39:11 -0300

    continuacao da aula de firebase com banco de dados em tempo real

---
+++ ShoppingList/ShoppingList/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  ShoppingList
+//
+//  Created by Leandro Romano on 05/10/18.
+//  Copyright © 2018 FIAP. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    var id: String = ""
+    var name: String = ""
+    var quantity: Int = 1
+}
--- ShoppingList/ShoppingList/TableViewController.swift
+++ ShoppingList/ShoppingList/TableViewController.swift
@@ -8,16 +8,80 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 class TableViewController: UITableViewController {
     
     var user: User!
+    var firestore: Firestore = {
+        let settings = FirestoreSettings()
+        settings.areTimestampsInSnapshotsEnabled = true
+        settings.isPersistenceEnabled = true
+        
+        let firestore = Firestore.firestore()
+        firestore.settings = settings
+        
+        return firestore
+    }()
+    var firestoreListener: ListenerRegistration!
+    var shoppingListCollection = "shoppingList"
+    var shoppingList: [ShoppingItem] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         user = Auth.auth().currentUser
         title = user.displayName
+        loadItems()
     }
+    
+    func loadItems() {
+        firestoreListener = firestore.collection(shoppingListCollection)
+            .order(by: "name")
+            .addSnapshotListener(includeMetadataChanges: true, listener: { (snapshot, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                } else {
+                    guard let snapshot = snapshot else { return }
+                    
+                    if snapshot.metadata.isFromCache || snapshot.documentChanges.count > 0 {
+                        self.showItems(snapshot: snapshot)
+                    }
+                }
+        })
+    }
+    
+    func showItems(snapshot: QuerySnapshot) {
+        shoppingList.removeAll()
+        
+        for document in snapshot.documents {
+            let data = document.data()
+            
+            let name = data["name"] as! String
+            let quantity = data["quantity"] as! Int
+            
+            let shoppingItem = ShoppingItem(id: document.documentID, name: name, quantity: quantity)
+            
+            shoppingList.append(shoppingItem)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        let data: [String: Any] = [
+            "name": "PARTIU PIZZINHA",
+            "quantity": 4
+        ]
+        
+        firestore.collection(shoppingListCollection).addDocument(data: data) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Item adicionado com sucesso.")
+            }
+        }
+    }
+    
     
     @IBAction func logout(_ sender: UIBarButtonItem) {
         do {
@@ -31,24 +95,23 @@ class TableViewController: UITableViewController {
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return shoppingList.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
+        let shoppingItem = shoppingList[indexPath.row]
+        
+        cell.textLabel?.text = shoppingItem.name
+        cell.detailTextLabel?.text = "\(shoppingItem.quantity)"
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -58,41 +121,22 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let shoppingItem = shoppingList[indexPath.row]
+            firestore.collection(shoppingListCollection).document(shoppingItem.id).delete()
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let shoppingItem = shoppingList[indexPath.row]
+        
+        let data: [String: Any] = [
+            "name": shoppingItem.name,
+            "quantity": Int(arc4random_uniform(30) + 1)
+        ]
+        
+        firestore.collection(shoppingListCollection).document(shoppingItem.id).updateData(data)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
